a000956dc69072ff9a7342ea7167376f61002abf
jccasfer <jccasfer@gmail.com>
2023-11-12 19:50:41 +0100

    Auth with Next Auth - 6 CredentialsProvider

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -3,13 +3,45 @@
 
 import NextAuth, { NextAuthOptions } from "next-auth"
 import GoogleProvider from "next-auth/providers/google";
+import CredentialsProvider from "next-auth/providers/credentials";
 import { PrismaAdapter } from "@next-auth/prisma-adapter";
 import prisma from "@/prisma/client";
+import bcrypt from 'bcrypt';
 
 //exportamos el objeto con las propiedades de la auth como una constante a parte para poder usarla desde fuera de este modulo
 export const authOptions: NextAuthOptions = {
     adapter: PrismaAdapter(prisma),
     providers: [
+        //Para usar credenciales propias guardadas en BD. This functionality is intentionally limited to discourage use of passwords due to the inherent security risks associated with them and the additional complexity associated with supporting usernames and passwords.
+        //https://next-auth.js.org/providers/credentials
+        CredentialsProvider({
+            // The name to display on the sign in form (e.g. "Sign in with...")
+            name: "Your Credentials",
+            // `credentials` is used to generate a form on the sign in page.
+            // You can specify which fields should be submitted, by adding keys to the `credentials` object.
+            // e.g. domain, username, password, 2FA token, etc.
+            // You can pass any HTML attribute to the <input> tag through the object.
+            credentials: {
+                email: { label: 'Email', type: 'email', placeholder: 'you@domain.com' },
+                password: { label: 'Password', type: 'password', placeholder: 'password' }
+            },
+            async authorize(credentials, req) {
+                // Add logic here to look up the user from the credentials supplied
+                if (!credentials?.email || !credentials.password) return null;
+
+                const user = await prisma.user.findUnique({
+                    where: { email: credentials.email }
+                });
+                if (!user) return null;
+                //bcrypt lo hemos añadido por npx i para ayudarnos con poder tener passwords encriptados
+                const passwordsMatch = await bcrypt.compare(
+                    credentials.password,
+                    user.hashedPassword!
+                );
+                return passwordsMatch ? user : null;
+            }
+        }),
+
         GoogleProvider({
             clientId: process.env.GOOGLE_CLIENT_ID!,           //! al final => si no está, nos marca como erroneo el valor de la vble ya que puede ser undefined, pero nosotros sabes que en el fichero .env está el valor, así que con ! para decirle al compilador typescript que no de error que sabemos que tiene valor
             clientSecret: process.env.GOOGLE_CLIENT_SECRET!
---
+++ app/api/register/route.tsx
@@ -0,0 +1,41 @@
+//API para registrar un usuario. Podemos usarla desde Postman
+// deberiamos hacer un registration form, pero eso no lo vemos ahora
+
+import prisma from "@/prisma/client";
+import { NextRequest, NextResponse } from "next/server";
+import { z } from "zod";
+import bcrypt from 'bcrypt';
+
+const schema = z.object({
+    email: z.string().email(),
+    password: z.string().min(5)
+});
+
+export async function POST(request: NextRequest) {
+    const body = await request.json();
+    const validation = schema.safeParse(body);
+    if (!validation.success) {
+        return NextResponse.json(validation.error.errors, {
+            status: 400
+        });
+    }
+
+    const user = await prisma.user.findUnique({
+        where: { email: body.email }
+    });
+    if (user) {
+        return NextResponse.json({ error: 'User already exist' }, {
+            status: 400
+        })
+    }
+
+    const hashedpass = await bcrypt.hash(body.password, 10);
+    const newUser = await prisma.user.create({
+        data: {
+            email: body.email,
+            hashedPassword: hashedpass
+        }
+    });
+
+    return NextResponse.json({ email: newUser.email });
+}

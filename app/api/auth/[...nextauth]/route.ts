291d29483c9eb49c9f6568cf341a5d5bbf824547
jccasfer <jccasfer@gmail.com>
2023-11-12 18:14:42 +0100

    Auth with Next Auth - 3 Access session from server

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -4,15 +4,16 @@
 import NextAuth from "next-auth"
 import GoogleProvider from "next-auth/providers/google";
 
-
-
-const handler = NextAuth({
+//exportamos el objeto con las propiedades de la auth como una constante a parte para poder usarla desde fuera de este modulo
+export const authOptions = {
     providers: [
         GoogleProvider({
-          clientId: process.env.GOOGLE_CLIENT_ID!,           //! al final => si no está, nos marca como erroneo el valor de la vble ya que puede ser undefined, pero nosotros sabes que en el fichero .env está el valor, así que con ! para decirle al compilador typescript que no de error que sabemos que tiene valor
-          clientSecret: process.env.GOOGLE_CLIENT_SECRET!
+            clientId: process.env.GOOGLE_CLIENT_ID!,           //! al final => si no está, nos marca como erroneo el valor de la vble ya que puede ser undefined, pero nosotros sabes que en el fichero .env está el valor, así que con ! para decirle al compilador typescript que no de error que sabemos que tiene valor
+            clientSecret: process.env.GOOGLE_CLIENT_SECRET!
         })]
-})
+};
+
+const handler = NextAuth(authOptions);
 
 export { handler as GET, handler as POST }
 
--- app/page.tsx
+++ app/page.tsx
@@ -2,16 +2,20 @@ import Image from 'next/image'
 import Link from 'next/link'
 import ProductCard from './components/ProductCart'
 import ProductCart from './components/ProductCart'
+import { getServerSession } from 'next-auth'
+import { authOptions } from './api/auth/[...nextauth]/route'
 
 /* Usamos Link Component de react para en vez
   de un a href para que no recargue todos los javascripts/fuentes... asociados a la pagina, 
   solo los del componente a ver*/
 
-export default function Home() {
+export default async function Home() {
+  const session = await getServerSession(authOptions); //getServerSession se puede usar en Pages y en route handlers(api)
+
   return (
     <main>
-      <h1>Hello World</h1>
-      <Link href="/users">Users</Link> 
+      <h1>Hello {session && <span>{session.user!.name}</span>}</h1>
+      <Link href="/users">Users</Link>
       <ProductCart />
     </main>
   )
